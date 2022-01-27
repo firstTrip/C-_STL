dacfa790038a441dedc0a801260f31d1ecfd07f1
firstTrip <hdnwls961@gmail.com>
2022-01-27 21:43:35 +0900

    연산자 오버라이딩 정의

---
+++ Ch.1 Overriding/Ch1_3/Ch1_3/Ch1_3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+class Point
+{
+	int x;
+	int y;
+
+	public :
+		Point(int _x = 0, int _y = 0) : x(_x), y(_y) {}
+		void Print() const { cout << x << ',' << y << endl; }
+
+};
+
+int main()
+{
+	Point p1(2, 3), p2(5, 5);
+
+	p1 + p2; // p1.operator+(p2);와 같은 의미
+
+	p1 * p2; // p1.operator*(p2);와 같은 의미
+
+	p1 = p2; // p1.operator=(p2);와 같은 의미
+
+	p1 == p2; // p1.operator==(p2);와 같은 의미
+
+	p1 += p2; // p1.operator+=(p2);와 같은 의미
+
+
+	return 0;
+}
